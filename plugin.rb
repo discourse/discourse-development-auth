e6c622d4901d808c7eb23d30673c34cf828967b2
David Taylor <david@taylorhq.com>
2021-03-19 13:40:17 +0000

    Add fake DiscourseConnect provider

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,8 @@ raise "discourse-development-auth is highly insecure and should not be installed
 
 enabled_site_setting :development_authentication_enabled
 
+PLUGIN_NAME = "discourse-development-auth"
+
 module ::OmniAuth
   module Strategies
     class Development
@@ -121,3 +123,94 @@ class DevelopmentAuthenticator < Auth::ManagedAuthenticator
 end
 
 auth_provider authenticator: DevelopmentAuthenticator.new
+
+
+### DiscourseConnect
+after_initialize do
+  module ::DevelopmentAuth
+    class Engine < ::Rails::Engine
+      engine_name PLUGIN_NAME
+      isolate_namespace ::DevelopmentAuth
+    end
+  end
+
+  class ::DevelopmentAuth::FakeDiscourseConnectController < ::ApplicationController
+    requires_plugin "discourse-development-auth"
+
+    skip_before_action :check_xhr, :preload_json, :redirect_to_login_if_required, :verify_authenticity_token
+
+    SIMPLE_FIELDS = %w{
+      external_id
+      email
+      username
+      name
+    }
+    ADVANCED_FIELDS = SingleSignOn::ACCESSORS.map(&:to_s) - SIMPLE_FIELDS
+    FIELDS = SIMPLE_FIELDS + ADVANCED_FIELDS
+
+    BOOLS = SingleSignOn::BOOLS.map(&:to_s)
+
+    COOKIE = "development-auth-discourseconnect-defaults"
+
+    def auth
+      raise "DISCOURSE_DEV_ALLOW_ANON_TO_IMPERSONATE must equal 1 to use this plugin" if ENV['DISCOURSE_DEV_ALLOW_ANON_TO_IMPERSONATE'] != "1"
+
+      params.require(:sso)
+      @payload = request.query_string
+      sso = SingleSignOn.parse(@payload, SiteSetting.discourse_connect_secret)
+
+      if request.method == "POST" && params[:external_id]
+        data = {}
+        FIELDS.each do |f|
+          sso.send(:"#{f}=", params[f])
+          data[f] = params[f]
+          cookies[COOKIE] = { value: data.to_json, path: "/", expires: 1.month.from_now }
+        end
+
+        return redirect_to sso.to_url(sso.return_sso_url)
+      end
+
+      raw_defaults = cookies[COOKIE] || "{}"
+      @defaults = JSON.parse(raw_defaults) rescue {}
+      @defaults["return_sso_url"] = sso.return_sso_url
+      @defaults["nonce"] = sso.nonce
+      @defaults["external_id"] = SecureRandom.hex(8) unless @defaults["external_id"].present?
+      render_form
+    end
+
+    private
+
+    def render_form
+      @simple_fields = SIMPLE_FIELDS
+      @advanced_fields = ADVANCED_FIELDS
+      @bools = BOOLS
+      append_view_path(File.expand_path("../app/views", __FILE__))
+      render template: "fake_discourse_connect/form", layout: false
+    end
+  end 
+
+  DevelopmentAuth::Engine.routes.draw do
+    get "/fake-discourse-connect" => "fake_discourse_connect#auth"
+    post "/fake-discourse-connect" => "fake_discourse_connect#auth"
+  end
+
+  Discourse::Application.routes.append do
+    mount ::DevelopmentAuth::Engine, at: "/development-auth"
+  end
+
+  DiscourseSingleSignOn.singleton_class.prepend(Module.new do
+    def sso_url
+      if SiteSetting.development_authentication_enabled
+        return "#{Discourse.base_path}/development-auth/fake-discourse-connect"
+      end
+      super
+    end
+  end)
+
+  EnableSsoValidator.prepend(Module.new do
+    def valid_value?(val)
+      return true if SiteSetting.development_authentication_enabled
+      super
+    end
+  end)
+end
